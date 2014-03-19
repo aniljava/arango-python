188f74f975aea9630c58be027ed51b6979368ef2
Anil <anil@aniljava.com>
2014-03-19 01:04:36 -0500

    Changed clients from singleton to instances per connection.

--- arango/clients/__init__.py
+++ arango/clients/__init__.py
@@ -10,13 +10,13 @@ __all__ = ("Client",)
 ISPY3 = sys.version_info >= (3, 0)
 
 logger = logging.getLogger("arango.client")
-Client = Urllib2Client
+Client = Urllib2Client()
 
 
 if ISPY3 is False:
     try:
         from .pycurlclient import PyCurlClient
-        Client = PyCurlClient
+        Client = PyCurlClient()
     except Exception as e:
         logger.warning(
             u"Sorry, can't import PyCurlClient. Reason: %s", str(e))
